bdabd048262cdae3b9a06c5c6a745a009384d54e
Alistair Walsh <alistair.walsh@me.com>
2022-11-04 13:29:50 +1100

    wrong spot

--- pages/form.py
+++ pages/form.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from pdf2image import convert_from_path, convert_from_bytes
 
-st.image(uploaded_file)
+
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 st.write(uploaded_file)
+st.image(uploaded_file)
 if uploaded_file is not None:
         images = convert_from_bytes(uploaded_file.read())
         for page in images:
