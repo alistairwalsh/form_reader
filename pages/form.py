ad0eed1ce6abe0969e3e7ed147214dae70503f16
Alistair Walsh <alistair.walsh@me.com>
2022-11-04 13:20:06 +1100

    bytes or file?

--- pages/form.py
+++ pages/form.py
@@ -3,7 +3,8 @@ from pdf2image import convert_from_path, convert_from_bytes
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 if uploaded_file is not None:
-    df = convert_from_bytes(uploaded_file)
+    with open(uploaded_file, 'rb') as infile:
+        images = convert_from_bytes(infile.read())
 
 
 # def extract_data(feed):
@@ -13,5 +14,3 @@ if uploaded_file is not None:
 #         for p in pages:
 #             data.append(p.extract_tables())
 #     return None # build more code to return a dataframe 
-
-images = convert_from_path(file)
