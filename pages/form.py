0c2d358cc3f878e0ce8726bf524899d18e03fc66
Alistair Walsh <alistair.walsh@me.com>
2022-11-04 13:16:57 +1100

    convert to image

--- pages/form.py
+++ pages/form.py
@@ -1,8 +1,9 @@
 import streamlit as st
+from pdf2image import convert_from_path, convert_from_bytes
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 if uploaded_file is not None:
-    df = extract_data(uploaded_file)
+    df = convert_from_bytes(uploaded_file)
 
 
 # def extract_data(feed):
