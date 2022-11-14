02d40967ae8df6fd6743670db6f4eba66aa087a1
Alistair Walsh <alistair.walsh@me.com>
2022-11-15 10:18:00 +1100

    test 2 tolml

--- pages/test_toml.py
+++ pages/test_toml.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import os
 
 st.write(st.secrets["test"])
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = st.secrets["test"]
+
+st.write(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
