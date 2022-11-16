75bdd2068c15a06ebfafc2033da1188e4d3aaa21
Alistair Walsh <alistair.walsh@me.com>
2022-11-16 12:38:19 +1100

    test as caps

--- pages/test_toml.py
+++ pages/test_toml.py
@@ -4,6 +4,6 @@ import os
 default_value = 'not found'
 st.write(st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]["type"])
 
-result = os.environ.get("test", default_value)
+result = os.environ.get("TEST", default_value)
 
 st.write( result)
